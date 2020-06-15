02b306251577648875a1121d1dff1b41913a1ed6
Jayraj Patel <Jayraj.Patel14@gmail.com>
2020-06-15 21:23:45 +0100

    Added Sound:

--- breath-app/script.js
+++ breath-app/script.js
@@ -1,9 +1,12 @@
 const container = document.querySelector(".container");
 const text = document.querySelector("#text");
+const music = document.querySelector("#music");
 
 const totalTime = 7500;
 const breathTime = (totalTime / 5) * 2;
 const holdTime = totalTime / 5;
+
+music.play();
 
 function bearthAnimation() {
   text.innerHTML = "Breath In!";
